5dc380ce7e6b9b889d4dbd9c8be06de962eb76ca
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-18 16:29:13 -0600

    Adding Actions and handlers with the storyboard

--- Journal/CreateEntry.swift
+++ Journal/CreateEntry.swift
@@ -10,6 +10,56 @@ import UIKit
 
 class CreateEntry: UIViewController {
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var addTagButton: UIButton!
     
+    @IBAction func addTag(){
+        presentAlertController()
+    }
+    
+    @IBAction func save(){
+        guard let title = titleTextField.text, !title.isEmpty,
+            let body = bodyTextView.text, !body.isEmpty else {return}
+        titleTextField.text = ""
+        bodyTextView.text = "Enter a body..."
+        addTagButton.setTitle("Add a tag", for: .normal)
+        
+        var tag: String? = nil
+        if addTagButton.titleLabel?.text != "Add a Tag" {
+            tag = addTagButton.titleLabel?.text
+        }
+        
+        EntryController.shared.createEntry(withTitle: title, body: body, tag: tag, color: .lightGray)
+        
+        resignResponders()
+    }
+    
+    func resignResponders(){
+        titleTextField.resignFirstResponder()
+        bodyTextView.resignFirstResponder()
+    }
+    
+}
+
+extension CreateEntry{
+    
+    func presentAlertController(){
+        let alertController = UIAlertController(title:"Tag", message:"Add a tag", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter a tag you wish to add..."
+        }
+        let dismissAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let addTagAction = UIAlertAction(title: "Add", style:.default){ (_) in
+            guard let textField = alertController.textFields?.first,
+                let tag = textField.text, !tag.isEmpty else {return}
+            self.addTagButton.setTitle(tag, for: .normal)
+        }
+        
+        alertController.addAction(dismissAction)
+        alertController.addAction(addTagAction)
+        
+        present(alertController, animated:true, completion:nil)
+    }
     
 }

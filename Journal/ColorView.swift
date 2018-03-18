681ae0a1c9c1e4c8d047f4aa7f3c3a3b73f94fae
Carlo Gilmar <carlogilmar12@gmail.com>
2018-03-18 02:45:48 -0600

    Adding git ignore

---
+++ Journal/ColorView.swift
@@ -0,0 +1,9 @@
+//
+//  ColorView.swift
+//  Journal
+//
+//  Created by Carlo Gilmar on 3/18/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import Foundation
---
+++ Journal/CreateEntry.swift
@@ -0,0 +1,9 @@
+//
+//  CreateEntry.swift
+//  Journal
+//
+//  Created by Carlo Gilmar on 3/18/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+
+import Foundation
